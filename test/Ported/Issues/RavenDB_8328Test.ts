3d380e55f82a8531d6e546c70fff1e2ee2b99cba
Marcin Lewandowski <marcin@ravendb.net>
2018-09-27 17:12:12 +0200

    tests for spatial sync with java: 8bba5372f0c1fcc3e5fb9610fa556ca40ae26330

---
+++ test/Ported/Issues/RavenDB_8328Test.ts
@@ -0,0 +1,97 @@
+import * as assert from "assert";
+import { testContext, disposeTestDocumentStore } from "../../Utils/TestUtil";
+
+import {
+    IDocumentStore,
+    PointField, WktField, QueryStatistics,
+} from "../../../src";
+
+describe("Issue RavenDB-8328", function () {
+
+    let store: IDocumentStore;
+
+    beforeEach(async function () {
+        store = await testContext.getDocumentStore();
+    });
+
+    afterEach(async () =>
+        await disposeTestDocumentStore(store));
+
+    it("can do spatial on auto index", async () => {
+        {
+            const session = store.openSession();
+            const item = new Item();
+            item.latitude = 10;
+            item.longitude = 20;
+            item.latitude2 = 10;
+            item.longitude2 = 20;
+            item.shapeWkt = "POINT(20 10)";
+            item.name = "Name1";
+            await session.store(item);
+            await session.saveChanges();
+        }
+
+        {
+            const session = store.openSession();
+            let q = session.query<Item>(Item)
+                .spatial(new PointField("latitude", "longitude"), f => f.withinRadius(10, 10, 20));
+            let iq = q.getIndexQuery();
+            assert.strictEqual(iq.query, "from Items " +
+                "where spatial.within(spatial.point(latitude, longitude), spatial.circle($p0, $p1, $p2))");
+
+            q = session.query<Item>(Item)
+                .spatial(new WktField("shapeWkt"), f => f.withinRadius(10, 10, 20));
+            iq = q.getIndexQuery();
+            assert.strictEqual(iq.query,
+                "from Items where spatial.within(spatial.wkt(shapeWkt), spatial.circle($p0, $p1, $p2))");
+        }
+
+        {
+            const session = store.openSession();
+
+            let stats = null as QueryStatistics;
+
+            let results = await session.query<Item>(Item)
+                .statistics(x => stats = x)
+                .spatial(new PointField("latitude", "longitude"), f => f.withinRadius(10, 10, 20))
+                .all();
+
+            assert.strictEqual(results.length, 1);
+            assert.strictEqual(stats.indexName, "Auto/Items/BySpatial.point(latitude|longitude)");
+
+            stats = null;
+
+            results = await session.query<Item>(Item)
+                .statistics(x => stats = x)
+                .spatial(new PointField("latitude2", "longitude2"), f => f.withinRadius(10, 10, 20))
+                .all();
+
+            assert.strictEqual(results.length, 1);
+            assert.strictEqual(stats.indexName,
+                "Auto/Items/BySpatial.point(latitude|longitude)AndSpatial.point(latitude2|longitude2)");
+
+            stats = null;
+
+            results = await session.query<Item>(Item)
+                .statistics(x => stats = x)
+                .spatial(new WktField("shapeWkt"), f => f.withinRadius(10, 10, 20))
+                .all();
+
+            assert.strictEqual(results.length, 1);
+            assert.strictEqual(stats.indexName,
+                "Auto/Items/BySpatial.point(latitude|longitude)" +
+                "AndSpatial.point(latitude2|longitude2)AndSpatial.wkt(shapeWkt)");
+        }
+    });
+
+});
+
+export class Item {
+    public id: string;
+    public name: string;
+    public latitude: number;
+    public longitude: number;
+    public latitude2: number;
+    public longitude2: number;
+    public shapeWkt;
+}
---
+++ test/Ported/Spatial/BoundingBoxIndexTest.ts
@@ -0,0 +1,149 @@
+import {AbstractIndexCreationTask} from "../../../src/Documents/Indexes";
+import {SpatialBounds} from "../../../src";
+
+import * as assert from "assert";
+import { testContext, disposeTestDocumentStore } from "../../Utils/TestUtil";
+
+import {
+    IDocumentStore
+} from "../../../src";
+
+describe("BoundingBoxIndexTest", function () {
+
+    let store: IDocumentStore;
+
+    beforeEach(async function () {
+        store = await testContext.getDocumentStore();
+    });
+
+    afterEach(async () =>
+        await disposeTestDocumentStore(store));
+
+    it("bounding box test", async () => {
+        const polygon = "POLYGON ((0 0, 0 5, 1 5, 1 1, 5 1, 5 5, 6 5, 6 0, 0 0))";
+        const rectangle1 = "2 2 4 4";
+        const rectangle2 = "6 6 10 10";
+        const rectangle3 = "0 0 6 6";
+
+        await new BBoxIndex().execute(store);
+        await new QuadTreeIndex().execute(store);
+
+        {
+            const session = store.openSession();
+            const doc = new SpatialDoc();
+            doc.shape = polygon;
+            await session.store(doc);
+            await session.saveChanges();
+        }
+
+        await testContext.waitForIndexing(store);
+
+        {
+            const session = store.openSession();
+            const result = await session.query(SpatialDoc).count();
+            assert.strictEqual(result, 1);
+        }
+
+        {
+            const session = store.openSession();
+            const result = await session.query<SpatialDoc>({
+                indexName: "BBoxIndex",
+                documentType: SpatialDoc
+            })
+                .spatial("shape", x => x.intersects(rectangle1))
+                .count();
+
+            assert.strictEqual(result, 1);
+        }
+
+        {
+            const session = store.openSession();
+            const result = await session.query<SpatialDoc>({
+                indexName: "BBoxIndex",
+                documentType: SpatialDoc
+            })
+                .spatial("shape", x => x.intersects(rectangle2))
+                .count();
+
+            assert.strictEqual(result, 0);
+        }
+
+        {
+            const session = store.openSession();
+            const result = await session.query<SpatialDoc>({
+                indexName: "BBoxIndex",
+                documentType: SpatialDoc
+            })
+                .spatial("shape", x => x.disjoint(rectangle2))
+                .count();
+
+            assert.strictEqual(result, 1);
+        }
+
+        {
+            const session = store.openSession();
+            const result = await session.query<SpatialDoc>({
+                indexName: "BBoxIndex",
+                documentType: SpatialDoc
+            })
+                .spatial("shape", x => x.within(rectangle3))
+                .count();
+
+            assert.strictEqual(result, 1);
+        }
+
+        {
+            const session = store.openSession();
+            const result = await session.query<SpatialDoc>({
+                indexName: "QuadTreeIndex",
+                documentType: SpatialDoc
+            })
+                .spatial("shape", x => x.intersects(rectangle2))
+                .count();
+
+            assert.strictEqual(result, 0);
+        }
+
+        {
+            const session = store.openSession();
+            const result = await session.query<SpatialDoc>({
+                indexName: "QuadTreeIndex",
+                documentType: SpatialDoc
+            })
+                .spatial("shape", x => x.intersects(rectangle1))
+                .count();
+
+            assert.strictEqual(result, 0);
+        }
+    });
+
+});
+
+export class SpatialDoc {
+    public id: string;
+    public shape: string;
+}
+
+export class BBoxIndex extends AbstractIndexCreationTask {
+    public constructor() {
+        super();
+
+        this.map = "docs.SpatialDocs.Select(doc => new {\n" +
+            "    shape = this.CreateSpatialField(doc.shape)\n" +
+            "})";
+
+        this.spatial("shape", x => x.cartesian().boundingBoxIndex());
+    }
+}
+
+export class QuadTreeIndex extends AbstractIndexCreationTask {
+    public constructor() {
+        super();
+
+        this.map = "docs.SpatialDocs.Select(doc => new {\n" +
+            "    shape = this.CreateSpatialField(doc.shape)\n" +
+            "})";
+
+        this.spatial("shape", x => x.cartesian().quadPrefixTreeIndex(6, new SpatialBounds(0, 0, 16, 16)));
+    }
+}
