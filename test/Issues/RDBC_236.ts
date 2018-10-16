f588ee66c83fc36012ab55bee468eaaaff227264
Grzegorz Lachowski <gregolsky@ravendb.net>
2018-10-16 11:17:21 +0200

    make date storage tests tz-agnostic

--- test/Issues/RDBC_236.ts
+++ test/Issues/RDBC_236.ts
@@ -10,8 +10,15 @@ import {
 } from "../../src";
 import { DateUtil } from "../../src/Utility/DateUtil";
 import { GetDocumentsCommand } from "../../src/Documents/Commands/GetDocumentsCommand";
+import { StringUtil } from "../../src/Utility/StringUtil";
 
-describe.only("DateUtil", function () {
+const LOCAL_TIMEZONE_OFFSET = -(new Date().getTimezoneOffset()); // getTimezoneOffset() returns reversed offset
+const LOCAL_TIMEZONE_OFFSET_HOURS = LOCAL_TIMEZONE_OFFSET / 60;
+const LOCAL_TIMEZONE_STRING =
+    // tslint:disable-next-line:max-line-length
+    `${LOCAL_TIMEZONE_OFFSET >= 0 ? "+" : "-"}${StringUtil.leftPad(LOCAL_TIMEZONE_OFFSET_HOURS.toString(), 2, "0")}:00`;
+
+describe("DateUtil", function () {
 
     describe("without timezones", function () {
 
@@ -33,9 +40,12 @@ describe.only("DateUtil", function () {
                 withTimezone: false,
                 useUtcDates: true
             });
-            const date = moment("2018-10-15T09:46:28.306").toDate();
+            const date = moment("2018-10-15T12:00:00.000").toDate();
             const stringified = dateUtil.stringify(date);
-            assert.strictEqual(stringified, "2018-10-15T07:46:28.3060000Z");
+
+            const expected = new Date(2018, 9, 15, date.getHours() - LOCAL_TIMEZONE_OFFSET_HOURS, 0, 0, 0);
+            const expectedStringified = moment(expected).format(DateUtil.DEFAULT_DATE_FORMAT) + "Z";
+            assert.strictEqual(stringified, expectedStringified);
 
             const parsed = dateUtil.parse(stringified);
             assert.strictEqual(parsed.getHours(), date.getHours());
@@ -50,11 +60,20 @@ describe.only("DateUtil", function () {
             const dateUtil = new DateUtil({
                 withTimezone: true
             });
-            const date = moment.parseZone("2018-10-15T09:46:28.3060000+06:00").toDate();
-            assert.strictEqual(date.getHours(), 5);
 
+            const hour6 = 12;
+            const timezoneOffsetHours = 6;
+            const date = moment.parseZone(`2018-10-15T${hour6}:00:00.0000000+06:00`).toDate();
+            // preconditions check
+            assert.strictEqual(
+                date.getHours(), hour6 - timezoneOffsetHours + LOCAL_TIMEZONE_OFFSET_HOURS);
+
+            const expectedHours = date.getHours();
+            const expected = new Date(2018, 9, 15, expectedHours, 0, 0, 0);
+            const expectedStringified = 
+                moment(expected).format(DateUtil.DEFAULT_DATE_FORMAT) + LOCAL_TIMEZONE_STRING;
             const stringified = dateUtil.stringify(date);
-            assert.strictEqual(stringified, "2018-10-15T05:46:28.3060000+02:00");
+            assert.strictEqual(stringified, expectedStringified);
 
             const parsed = dateUtil.parse(stringified);
             assert.strictEqual(parsed.getHours(), date.getHours());
@@ -66,11 +85,21 @@ describe.only("DateUtil", function () {
                 withTimezone: true,
                 useUtcDates: true
             });
-            const date = moment.parseZone("2018-10-15T09:46:28.3060000+06:00").toDate();
-            assert.strictEqual(date.getHours(), 5);
 
+            const hour6 = 12;
+            const timezoneOffsetHours = 6;
+            const date = moment.parseZone(`2018-10-15T${hour6}:00:00.0000000+06:00`).toDate();
+            // preconditions check
+            assert.strictEqual(
+                date.getHours(), hour6 - timezoneOffsetHours + LOCAL_TIMEZONE_OFFSET_HOURS);
+
+            const expectedHours = date.getHours() - LOCAL_TIMEZONE_OFFSET_HOURS;
+            const utcTimezoneString = "+00:00";
+            const expected = new Date(2018, 9, 15, expectedHours, 0, 0, 0);
+            const expectedStringified = 
+                moment(expected).format(DateUtil.DEFAULT_DATE_FORMAT) + utcTimezoneString;
             const stringified = dateUtil.stringify(date);
-            assert.strictEqual(stringified, "2018-10-15T03:46:28.3060000+00:00");
+            assert.strictEqual(stringified, expectedStringified);
 
             const parsed = dateUtil.parse(stringified);
             assert.strictEqual(parsed.getHours(), date.getHours());
@@ -85,7 +114,7 @@ describe("[RDBC-236] Dates storage", function () {
 
     let store: IDocumentStore;
 
-    describe("store timezone info", function () {
+    describe("storing timezone info", function () {
 
         beforeEach(async function () {
             testContext.customizeStore = async store => {
@@ -102,8 +131,9 @@ describe("[RDBC-236] Dates storage", function () {
         afterEach(async () =>
             await disposeTestDocumentStore(store));
 
-        it("can store & load date with timezone info", async () => {
-            const date = new Date(2018, 9, 12, 13, 10, 10, 0);
+        it("can store & load date", async () => {
+            const hoursLocal = 13;
+            const date = new Date(2018, 9, 12, hoursLocal, 10, 10, 0);
 
             {
                 const session = store.openSession();
@@ -122,7 +152,7 @@ describe("[RDBC-236] Dates storage", function () {
                 });
                 await store.getRequestExecutor().execute(cmd);
                 assert.strictEqual(
-                    cmd.result.results[0]["start"], "2018-10-12T13:10:10.0000000+02:00");
+                    cmd.result.results[0]["start"], "2018-10-12T13:10:10.0000000" + LOCAL_TIMEZONE_STRING);
             }
 
             {
@@ -154,7 +184,8 @@ describe("[RDBC-236] Dates storage", function () {
             await disposeTestDocumentStore(store));
 
         it("can properly store & load date", async () => {
-            const date = new Date(2018, 9, 12, 13, 10, 10, 0);
+            const hoursLocal = 13;
+            const date = new Date(2018, 9, 12, hoursLocal, 10, 10, 0);
 
             {
                 const session = store.openSession();
@@ -172,8 +203,10 @@ describe("[RDBC-236] Dates storage", function () {
                     conventions: store.conventions
                 });
                 await store.getRequestExecutor().execute(cmd);
+                const hoursUtcString = StringUtil.leftPad(
+                    (hoursLocal - LOCAL_TIMEZONE_OFFSET_HOURS).toString(), 2, "0");
                 assert.strictEqual(
-                    cmd.result.results[0]["start"], "2018-10-12T11:10:10.0000000Z");
+                    cmd.result.results[0]["start"], `2018-10-12T${hoursUtcString}:10:10.0000000Z`);
             }
 
             {
@@ -205,8 +238,9 @@ describe("[RDBC-236] Dates storage", function () {
         afterEach(async () =>
             await disposeTestDocumentStore(store));
 
-        it("can store & load date with timezone info", async () => {
-            const date = new Date(2018, 9, 12, 13, 10, 10, 0);
+        it("can store & load date", async () => {
+            const hoursLocal = 13;
+            const date = new Date(2018, 9, 12, hoursLocal, 10, 10, 0);
 
             {
                 const session = store.openSession();
@@ -224,8 +258,11 @@ describe("[RDBC-236] Dates storage", function () {
                     conventions: store.conventions
                 });
                 await store.getRequestExecutor().execute(cmd);
+                const hoursUtcString = StringUtil.leftPad(
+                    (hoursLocal - LOCAL_TIMEZONE_OFFSET_HOURS).toString(), 2, "0");
                 assert.strictEqual(
-                    cmd.result.results[0]["start"], "2018-10-12T11:10:10.0000000+00:00");
+                    cmd.result.results[0]["start"], 
+                    `2018-10-12T${hoursUtcString}:10:10.0000000+00:00`);
             }
 
             {
