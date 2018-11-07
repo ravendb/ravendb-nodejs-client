f88b7ed02a4648f7e1c6af81f71bdfd3da7fff13
Grzegorz Lachowski <gregolsky@ravendb.net>
2018-11-07 09:16:30 +0100

    detect index type test

--- test/Ported/Indexing/DetectIndexTypeTest.ts
+++ test/Ported/Indexing/DetectIndexTypeTest.ts
@@ -7,70 +7,45 @@ import {
     RavenErrorType,
     GetNextOperationIdCommand,
     IDocumentStore,
+    IndexType,
+    IndexDefinition,
 } from "../../../src";
 
 describe.only("DetectIndexTypeTest", function () {
 
-    let store: IDocumentStore;
-
-    beforeEach(async function () {
-        store = await testContext.getDocumentStore();
+    it("testValidMap", async () => {
+        const map = "from task in docs.Tasks select new { task.assigneeId }";
+        assert.strictEqual(findIndexType(map), "Map");
     });
 
-    afterEach(async () => 
-        await disposeTestDocumentStore(store));
-
-    it("can ", async () => {
-        assert.fail("do me next");
+    it("testMapSpaces", function () {
+        const map = "   from task in docs.Tasks select new { task.assigneeId }";
+        assert.strictEqual(findIndexType(map), "Map");
     });
+
+    it("testMapTabsBreaks", function () {
+        const map = "\r\r\t\r\n  from task in docs.Tasks select new { task.assigneeId }";
+        assert.strictEqual(findIndexType(map), "Map" as IndexType);
+    });
+
+    it("testMapComments", () => {
+        const map = "// this is comment \r\n from task in docs.Tasks select new { task.assigneeId }";
+        assert.strictEqual(findIndexType(map), "Map" as IndexType);
+    });
+
+    it("testJsMap", () => {
+        const map = "map('Users', x => x)";
+        assert.strictEqual(findIndexType(map), "JavaScriptMap" as IndexType);
+    });
+
+    it("testJsMapComment", () => {
+        const map = "//this is test\r\n  map('Users', x => x)";
+        assert.strictEqual(findIndexType(map), "JavaScriptMap" as IndexType);
+    });
+
+    function findIndexType(map: string): IndexType {
+        const indexDefinition = new IndexDefinition();
+        indexDefinition.maps = new Set([map]);
+        return indexDefinition.type;
+    }
 });
-
-// package net.ravendb.client.test.client.indexing;
-//  import com.google.common.collect.Sets;
-// import net.ravendb.client.documents.indexes.IndexDefinition;
-// import net.ravendb.client.documents.indexes.IndexType;
-// import org.junit.jupiter.api.Test;
-//  import static org.assertj.core.api.Assertions.assertThat;
-//  public class DetectIndexTypeTest {
-//      @Test
-//     public void testValidMap() {
-//         String map = "from task in docs.Tasks select new { task.assigneeId }";
-//          assertThat(findIndexType(map))
-//                 .isEqualTo(IndexType.MAP);
-//     }
-//      @Test
-//     public void testMapSpaces() {
-//         String map = "   from task in docs.Tasks select new { task.assigneeId }";
-//          assertThat(findIndexType(map))
-//                 .isEqualTo(IndexType.MAP);
-//     }
-//      @Test
-//     public void testMapTabsBreaks() {
-//         String map = " \r\r  \r\n  from task in docs.Tasks select new { task.assigneeId }";
-//          assertThat(findIndexType(map))
-//                 .isEqualTo(IndexType.MAP);
-//     }
-//      @Test
-//     public void testMapComments() {
-//         String map = "// this is comment \r\n from task in docs.Tasks select new { task.assigneeId }";
-//          assertThat(findIndexType(map))
-//                 .isEqualTo(IndexType.MAP);
-//     }
-//      @Test
-//     public void testJsMap() {
-//         String map = "map('Users', x => x)";
-//          assertThat(findIndexType(map))
-//                 .isEqualTo(IndexType.JAVA_SCRIPT_MAP);
-//     }
-//      @Test
-//     public void testJsMapComment() {
-//         String map = "//this is test\r\n  map('Users', x => x)";
-//          assertThat(findIndexType(map))
-//                 .isEqualTo(IndexType.JAVA_SCRIPT_MAP);
-//     }
-//      private static IndexType findIndexType(String map) {
-//         IndexDefinition indexDefinition = new IndexDefinition();
-//         indexDefinition.setMaps(Sets.newHashSet(map));
-//          return indexDefinition.getType();
-//      }
-// }
