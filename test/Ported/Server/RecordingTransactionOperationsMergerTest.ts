069937526daf86d52ce83ed1f082ccd8900625d9
Marcin Lewandowski <marcin@ravendb.net>
2020-07-12 12:56:16 +0200

    sync with java fb9519b888d46af6262041c3758fd2c01bfda024: create sample data in record transaction tests

--- test/Ported/Server/RecordingTransactionOperationsMergerTest.ts
+++ test/Ported/Server/RecordingTransactionOperationsMergerTest.ts
@@ -2,10 +2,10 @@ import { IDocumentStore } from "../../../src";
 import { disposeTestDocumentStore, TemporaryDirContext, testContext } from "../../Utils/TestUtil";
 import * as path from "path";
 import { StartTransactionsRecordingOperation } from "../../../src/Documents/Operations/TransactionsRecording/StartTransactionsRecordingOperation";
-import { User } from "../../Assets/Entities";
 import { StopTransactionsRecordingOperation } from "../../../src/Documents/Operations/TransactionsRecording/StopTransactionsRecordingOperation";
 import { assertThat } from "../../Utils/AssertExtensions";
 import * as fs from "fs";
+import { CreateSampleDataOperation } from "../../Utils/CreateSampleDataOperation";
 
 describe("RecordingTransactionOperationsMergerTest", function () {
 
@@ -28,9 +28,7 @@ describe("RecordingTransactionOperationsMergerTest", function () {
         await store.maintenance.send(new StartTransactionsRecordingOperation(path.resolve(targetFile)));
 
         try {
-            const session = store.openSession();
-            await session.store(new User());
-            await session.saveChanges()
+            await store.maintenance.send(new CreateSampleDataOperation());
         } finally {
             await store.maintenance.send(new StopTransactionsRecordingOperation());
         }
