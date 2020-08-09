5ce0fdcde7fc342ec7d6b4e578e62ff09380759d
Marcin Lewandowski <marcin@ravendb.net>
2020-08-09 22:56:14 +0200

    sync with java 157d6fe0b21db3c5e71e34ebc1f0433821a40733: sync with 4.2: 36b90f1e6f372f44d5d1bac04d0e0c416d341315 - @refresh feature

---
+++ test/Ported/Issues/RavenDB_13735.ts
@@ -0,0 +1,76 @@
+import { IDocumentStore } from "../../../src/Documents/IDocumentStore";
+import { disposeTestDocumentStore, testContext } from "../../Utils/TestUtil";
+import { RefreshConfiguration } from "../../../src/Documents/Operations/Refresh/RefreshConfiguration";
+import { ConfigureRefreshOperation } from "../../../src/Documents/Operations/Refresh/ConfigureRefreshOperation";
+import { User } from "../../Assets/Entities";
+import moment = require("moment");
+import { Stopwatch } from "../../../src/Utility/Stopwatch";
+import { throwError } from "../../../src/Exceptions/index";
+import { assertThat } from "../../Utils/AssertExtensions";
+import { delay } from "../../../src/Utility/PromiseUtil";
+
+describe("RavenDB_13735", function () {
+
+    let store: IDocumentStore;
+
+    beforeEach(async function () {
+        store = await testContext.getDocumentStore();
+    });
+
+    afterEach(async () =>
+        await disposeTestDocumentStore(store));
+
+    it("refreshWillUpdateDocumentChangeVector", async () => {
+        await setupRefresh(store);
+
+        let expectedChangeVector: string;
+        {
+            const session = store.openSession();
+            const user = Object.assign(new User(), {
+                name: "Oren"
+            });
+
+            await session.store(user, "users/1-A");
+
+            const hourAgo = moment().add(-1, "hour");
+
+            session.advanced.getMetadataFor(user)["@refresh"] = NetISO8601Utils.format(hourAgo);
+
+            await session.saveChanges();
+
+            expectedChangeVector = session.advanced.getChangeVectorFor(user);
+        }
+
+        const sw = Stopwatch.createStarted();
+
+        while (true) {
+            if (sw.elapsed > 10_000) {
+                throwError("TimeoutException");
+            }
+
+            {
+                const session = store.openSession();
+                const user = await session.load<User>("users/1-A", User);
+                assertThat(user)
+                    .isNotNull();
+
+                if (session.advanced.getChangeVectorFor(user) !== expectedChangeVector) {
+                    // change vector was changed - great!
+                    break;
+                }
+            }
+
+            await delay(200);
+        }
+    });
+
+});
+
+async function setupRefresh(store: IDocumentStore) {
+    const config: RefreshConfiguration = {
+        disabled: false,
+        refreshFrequencyInSec: 1
+    };
+
+    return await store.maintenance.send(new ConfigureRefreshOperation(config));
+}
