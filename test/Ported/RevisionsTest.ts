f9f99ac806b6c93be9c77a15411e78d03327fc5a
Marcin Lewandowski <marcin@ravendb.net>
2020-09-07 22:10:32 +0200

    sync with java c141d8473d98a8f67974ef028faf151a19164dc8: sync with .net: 60313b8c4c87c684c8c46141d6ad590595120850  - get all revisions

--- test/Ported/RevisionsTest.ts
+++ test/Ported/RevisionsTest.ts
@@ -3,11 +3,12 @@ import { testContext, disposeTestDocumentStore } from "../Utils/TestUtil";
 
 import { DocumentStore, IDocumentStore } from "../../src";
 import { User } from "../Assets/Entities";
-import { CONSTANTS } from "../../src/Constants";
 // tslint:disable:max-line-length
 import { ConfigureRevisionsOperationResult } from "../../src/Documents/Operations/Revisions/ConfigureRevisionsOperation";
 // tslint:enable:max-line-length
 import { GetRevisionsBinEntryCommand } from "../../src/Documents/Commands/GetRevisionsBinEntryCommand";
+import { Company } from "../Assets/Orders";
+import { assertThat } from "../Utils/AssertExtensions";
 
 describe("RevisionsTest", function () {
 
@@ -131,5 +132,49 @@ describe("RevisionsTest", function () {
         const result = revisionsBinEntryCommand.result;
         assert.strictEqual(result.results.length, 1);
         assert.strictEqual(result.results[0]["@metadata"]["@id"], "users/1");
+    });
+
+    it("canGetRevisionsByChangeVectors", async () => {
+        const id = "users/1";
+
+        await testContext.setupRevisions(store, false, 100);
+
+        {
+            const session = store.openSession();
+            const user = Object.assign(new User(), {
+                name: "Fitzchak"
+            });
+            await session.store(user, id);
+            await session.saveChanges();
+        }
+
+        for (let i = 0; i < 10; i++) {
+            const session = store.openSession();
+            const user = await session.load<Company>(id, Company);
+            user.name = "Fizchak" + i;
+            await session.saveChanges();
+        }
+
+        {
+            const session = store.openSession();
+            const revisionsMetadata = await session
+                .advanced
+                .revisions
+                .getMetadataFor(id);
+            assertThat(revisionsMetadata)
+                .hasSize(11);
+
+            const changeVectors = revisionsMetadata
+                .map(x => x["@change-vector"]);
+
+            changeVectors.push("NotExistsChangeVector");
+
+            const revisions = await session.advanced.revisions.get<User>(changeVectors, User);
+            assertThat(revisions["NotExistsChangeVector"])
+                .isNull();
+
+            assertThat(await session.advanced.revisions.get<User>("NotExistsChangeVector", User))
+                .isNull();
+        }
     });
 });
