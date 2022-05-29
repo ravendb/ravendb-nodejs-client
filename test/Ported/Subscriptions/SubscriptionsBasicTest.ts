c7ac46ea8daa622ee41daa7250fa23202214f725
Marcin Lewandowski <marcin@ravendb.net>
2022-05-29 20:49:02 +0200

    rollback subscription tests assertions

--- test/Ported/Subscriptions/SubscriptionsBasicTest.ts
+++ test/Ported/Subscriptions/SubscriptionsBasicTest.ts
@@ -647,13 +647,14 @@ describe("SubscriptionsBasicTest", function () {
         assert.ok(await docs.poll(_reasonableWaitTime));
         assert.ok(await docs.poll(_reasonableWaitTime));
 
-        await store.subscriptions.dropConnection(id);
-
-        await new Promise<void>( resolve => {
+        // eslint-disable-next-line no-async-promise-executor
+        await new Promise<void>( async resolve => {
             subscription.on("error", error => {
                 assert.strictEqual(error.name, "SubscriptionClosedException");
                 resolve();
             });
+
+            await store.subscriptions.dropConnection(id);
         });
 
         {
