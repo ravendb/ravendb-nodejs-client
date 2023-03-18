806615a42fd2cd7a4be5d1760b8895b238baffb0
Marcin Lewandowski <marcin@ravendb.net>
2023-03-18 20:41:25 +0100

    RDBC-654 Investigate cloudflare and node.js client interoperability

--- src/Primitives/Http.ts
+++ src/Primitives/Http.ts
@@ -1,6 +1,11 @@
 import { Request, RequestInit, Response } from "node-fetch";
 
-export type HttpRequestParameters = RequestInit & { uri: string };
-export type HttpRequestParametersWithoutUri = RequestInit;
+export type HttpRequestParameters = RequestInit & {
+  uri: string;
+  fetcher?: any;
+};
+export type HttpRequestParametersWithoutUri = RequestInit & {
+  fetcher?: any;
+};
 export type HttpResponse = Response;
 export type HttpRequest = Request;
