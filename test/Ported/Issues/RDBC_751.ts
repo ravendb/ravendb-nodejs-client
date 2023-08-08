37eeaa7db605d951a5c3b62a3455407d1794c60e
danielle9897 <danielle.9897@gmail.com>
2023-08-08 14:43:23 +0300

    RDBC-751 fix store dispose

--- test/Ported/Issues/RDBC_751.ts
+++ test/Ported/Issues/RDBC_751.ts
@@ -23,86 +23,93 @@ describe("RDBC_751", function () {
     }
 
     it("canSearchWithProximityZero", async () => {
-        const store = getStoreWithCustomConventions((s) => {
-            s.conventions.findCollectionNameForObjectLiteral = (o) => o["collection"];
-            s.conventions.entityFieldNameConvention = "camel";
-            s.conventions.remoteEntityFieldNameConvention = "pascal";
-            s.conventions.identityProperty = "id";
-            s.conventions.registerEntityIdPropertyName(Object, "id");
-        });
+        let store: DocumentStore;
 
-        await store.maintenance.send(new CreateSampleDataOperation());
-        
-        {
-            const session = store.openSession();
-            
-            const employee9A= await session.load<Employee>("employees/9-A");
-            employee9A.notes = ["Anne has a BA degree in English from St. Lawrence College. She has fluent French."];
-            await session.store(employee9A);
-            await session.saveChanges();
-        }
+        try {
+            store = getStoreWithCustomConventions((s) => {
+                s.conventions.findCollectionNameForObjectLiteral = (o) => o["collection"];
+                s.conventions.entityFieldNameConvention = "camel";
+                s.conventions.remoteEntityFieldNameConvention = "pascal";
+                s.conventions.identityProperty = "id";
+                s.conventions.registerEntityIdPropertyName(Object, "id");
+            });
 
-        // Test for terms that are maximum 5 terms apart
-        {
-            const session = store.openSession();
+            await store.maintenance.send(new CreateSampleDataOperation());
 
-            const employeesWithProximity5 = await session
-                .query(Employee)
-                .search("Notes", "fluent french")
-                .proximity(5)
-                .waitForNonStaleResults()
-                .all();
+            {
+                const session = store.openSession();
 
-            assertThat(employeesWithProximity5).hasSize(4);
-        }
+                const employee9A = await session.load<Employee>("employees/9-A");
+                employee9A.notes = ["Anne has a BA degree in English from St. Lawrence College. She has fluent French."];
+                await session.store(employee9A);
+                await session.saveChanges();
+            }
 
-        // Test for terms that are 0 terms apart
-        // Results will contain a single word that was not tokenized to a term in between
-        {
-            const session = store.openSession();
+            // Test for terms that are maximum 5 terms apart
+            {
+                const session = store.openSession();
 
-            const employeesWithProximity0 = await session
-                .query(Employee)
-                .search("Notes", "fluent french")
-                .proximity(0)
-                .waitForNonStaleResults()
-                .all();
+                const employeesWithProximity5 = await session
+                    .query(Employee)
+                    .search("Notes", "fluent french")
+                    .proximity(5)
+                    .waitForNonStaleResults()
+                    .all();
 
-            assertThat(employeesWithProximity0).hasSize(3);
-            
-            assertThat(employeesWithProximity0[0].id).isEqualTo("employees/2-A");
-            assertThat(employeesWithProximity0[1].id).isEqualTo("employees/5-A");
-            assertThat(employeesWithProximity0[2].id).isEqualTo("employees/9-A");
-            
-            assertThat(employeesWithProximity0[0].notes[0]).contains("fluent in French");
-            assertThat(employeesWithProximity0[1].notes[0]).contains("fluent in French");
-            assertThat(employeesWithProximity0[2].notes[0]).contains("fluent French");
-        }
+                assertThat(employeesWithProximity5).hasSize(4);
+            }
 
-        // Test that are 0 terms apart
-        // Consecutive results only
-        {
-            const session = store.openSession();
+            // Test for terms that are 0 terms apart
+            // Results will contain a single word that was not tokenized to a term in between
+            {
+                const session = store.openSession();
 
-            const employee2A= await session.load<Employee>("employees/2-A");
-            employee2A.notes = ["Andrew knows fluent Belgian French."];
-            await session.store(employee2A);
+                const employeesWithProximity0 = await session
+                    .query(Employee)
+                    .search("Notes", "fluent french")
+                    .proximity(0)
+                    .waitForNonStaleResults()
+                    .all();
 
-            const employee5A= await session.load<Employee>("employees/5-A");
-            employee5A.notes = ["Steven knows fluent Canadian French."];
-            await session.store(employee5A);
-            
-            await session.saveChanges();
+                assertThat(employeesWithProximity0).hasSize(3);
 
-            const employeesWithProximity0 = await session
-                .query(Employee)
-                .search("Notes", "fluent french")
-                .proximity(0)
-                .waitForNonStaleResults()
-                .all()
-            
-            assertThat(employeesWithProximity0).hasSize(1);
-            assertThat(employeesWithProximity0[0].id).isEqualTo("employees/9-A");
+                assertThat(employeesWithProximity0[0].id).isEqualTo("employees/2-A");
+                assertThat(employeesWithProximity0[1].id).isEqualTo("employees/5-A");
+                assertThat(employeesWithProximity0[2].id).isEqualTo("employees/9-A");
+
+                assertThat(employeesWithProximity0[0].notes[0]).contains("fluent in French");
+                assertThat(employeesWithProximity0[1].notes[0]).contains("fluent in French");
+                assertThat(employeesWithProximity0[2].notes[0]).contains("fluent French");
+            }
+
+            // Test that are 0 terms apart
+            // Consecutive results only
+            {
+                const session = store.openSession();
+
+                const employee2A = await session.load<Employee>("employees/2-A");
+                employee2A.notes = ["Andrew knows fluent Belgian French."];
+                await session.store(employee2A);
+
+                const employee5A = await session.load<Employee>("employees/5-A");
+                employee5A.notes = ["Steven knows fluent Canadian French."];
+                await session.store(employee5A);
+
+                await session.saveChanges();
+
+                const employeesWithProximity0 = await session
+                    .query(Employee)
+                    .search("Notes", "fluent french")
+                    .proximity(0)
+                    .waitForNonStaleResults()
+                    .all()
+
+                assertThat(employeesWithProximity0).hasSize(1);
+                assertThat(employeesWithProximity0[0].id).isEqualTo("employees/9-A");
+            }
+
+        } finally {
+            store.dispose();
         }
     });
 
